ba8fda242fe9b904105d186527558493eb7b4169
Vitalii Nedzelskyi <vitalii.nedzelskyi@gmail.com>
2016-03-30 15:23:07 +0300

    Write user information to file and chak if user present was added

---
+++ src/com/homebug/model/Run.java
@@ -0,0 +1,21 @@
+package com.homebug.model;
+
+import com.homebug.model.Login.Login;
+import com.homebug.model.User.Users;
+
+import java.io.IOException;
+
+/**
+ * This class just to test features
+ */
+public class Run {
+    public static void main(String[] args) throws IOException {
+        Users user = new Users("Vitalii", "C", "asdfadsf", 456965L, "password", true);
+        Login log = new Login();
+        if (log.isUserExist("Vitalii1")) {
+            System.out.println("ture");
+        }else {
+            System.out.println("false");
+        }
+    }
+}
--- src/com/homebug/model/User/Users.java
+++ src/com/homebug/model/User/Users.java
@@ -1,5 +1,7 @@
 package com.homebug.model.User;
 
+import java.io.*;
+
 /**
  * This class is for describe all kind of users and their main
  * functionality
@@ -25,13 +27,35 @@ public class Users {
         this.fName = fName;
     }
 
-    public Users(String fName, String lName, String address, long phone, String password, boolean admin) {
+    public Users(String fName, String lName, String address, long phone, String password, boolean admin) throws IOException {
         this.fName = fName;
         this.lName = lName;
         this.address = address;
         this.phone = phone;
         this.password = password;
         this.admin = admin;
+
+        File newUser = new File("E:\\HomeBug\\Users\\" + fName + ".txt" );
+        FileWriter fw = new FileWriter(newUser);
+        BufferedWriter writer = new BufferedWriter(fw);
+        try {
+            if (!newUser.exists()){
+                newUser.createNewFile();
+            } else {
+                writer.write("First Name: " + fName + "\n\r\n\r");
+                writer.write("Last Name: " + lName + "\n\r\n\r");
+                writer.write("Address Name: " + address + "\n\r\n\r");
+                writer.write("Password: " + password + "\n\r\n\r");
+                writer.write("Admin: " + admin + "\n\r\n\r");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            writer.flush();
+            writer.close();
+        }
     }
 
     protected void makeAdmin() {
